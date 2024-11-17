0f6439e27e4e43fbf10828fc3f4772660cf91347
ryan-errando <errandoryan@gmail.com>
2024-11-17 17:22:14 +0700

    first commit

---
+++ database/migrations/2024_11_17_100701_create_sellers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('password');
+            $table->string('address');
+            $table->string('shop_name');
+            $table->text('description');
+            $table->decimal('rating');
+            $table->boolean('is_verified');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sellers');
+    }
+};
