2d5d8724b1dddbe808755563fa27b599c9ec11e6
ryan-errando <errandoryan@gmail.com>
2024-12-14 21:23:02 +0700

    fixing error in regist

--- database/migrations/2024_11_21_070053_create_users_table.php
+++ database/migrations/2024_11_21_070053_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->text('address');
+            $table->text('phone')->nullable();
             $table->string('profile_picture')->nullable();
             $table->timestamps();
         });
--- database/migrations/2024_11_21_070222_create_sellers_table.php
+++ database/migrations/2024_11_21_070222_create_sellers_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('address');
-            $table->string('phone');
+            $table->string('phone')->nullable();
             $table->string('image')->nullable();
             $table->timestamps();
         });
